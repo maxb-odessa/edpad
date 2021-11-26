acaf27ad0bee998e33ce8bc1dc5a0383d86112b9
Max V. Buglakov <maxb@tutamail.com>
2021-11-26 23:17:09 +0200

    edsm+

---
+++ edsm/edsm.go
@@ -0,0 +1,44 @@
+package edsm
+
+import "edpad/log"
+
+type EDSM struct {
+	cmdrName string
+	apiKey   string
+	apiURL   string
+	softName string
+	softVer  string
+	ch       chan string
+	inited   bool
+}
+
+func Init() *EDSM {
+	e := new(EDSM)
+
+	// init, check conn etc
+
+	e.ch = make(chan string, 32)
+	go e.sendData()
+
+	return e
+}
+
+func (e *EDSM) Send(evType string, text string) {
+
+	if !e.inited {
+		return
+	}
+
+	if !acceptEvType(evType) {
+		return
+	}
+
+	e.ch <- text
+}
+
+func (e *EDSM) sendData() {
+	select {
+	case text := <-e.cc:
+		log.Debug("EDSM: %s\n", text)
+	}
+}
